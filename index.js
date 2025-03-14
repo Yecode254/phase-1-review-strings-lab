7672a0841e59c2a9813933e3d8f2ba7397e1c8bd
Yego <eddieyego273@gmail.com>
2025-03-14 13:53:46 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser=`Yego`
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, YEGO!`
+let shortGreeting = "Welcome, Y!"
